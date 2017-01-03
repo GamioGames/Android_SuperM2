cd0fbe65b315a6cbcbe64fc8b0b36b025f08b3ba
antony999k <lv999k@hotmail.com>
2017-01-03 16:26:56 -0600

    Android Studio, Ejercicio 02/Solución 02. Crear lista

---
+++ S02_Solucion_DesplegarLista/app/src/main/java/com/ejemplo/android/s02_solucion_desplegarlista/ListaSuper.java
@@ -0,0 +1,38 @@
+package com.ejemplo.android.s02_solucion_desplegarlista;
+
+/**
+ * Created by antony999k on 1/3/17.
+ */
+
+public class ListaSuper {
+    public static String[] obtenerListado(){
+        return new String[] {
+                "manzana",
+                "moras",
+                "fresa",
+                "papaya",
+                "Lechuga",
+                "Pepino",
+                "Cebolla",
+                "Pan molde",
+                "Arroz",
+                "Quinoa",
+                "Harina de trigo",
+                "Harina de maiz",
+                "Aceite de oliva",
+                "Mantequilla",
+                "Cilantro",
+                "Romero",
+                "Perejil",
+                "Vinagre",
+                "Refresco",
+                "Pasta penne",
+                "Jamón de pavo",
+                "Carne molida",
+                "Bistek magro",
+                "Pollo",
+                "Pescado",
+                "Atún enlatado"
+        };
+    }
+}
--- S02_Solucion_DesplegarLista/app/src/main/java/com/ejemplo/android/s02_solucion_desplegarlista/MainActivity.java
+++ S02_Solucion_DesplegarLista/app/src/main/java/com/ejemplo/android/s02_solucion_desplegarlista/MainActivity.java
@@ -1,4 +1,4 @@
-package com.ejemplo.android.s02_ejercicio_desplegarlista;
+package com.ejemplo.android.s02_solucion_desplegarlista;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,13 +7,20 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     //TODO (1) Declarar una variable de tipo TextView que lleve el nombre de listaSuperTextView
+    TextView listaSuperTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         // TODO (3) Usar findViewByID para relacionar el TextView del layout con findViewById()
+        listaSuperTextView = (TextView) findViewById(R.id.super_nombres);
         // TODO (4) Usa el metodo ListaSuper.obtenerListado y guarda los datos en un arreglo de Strings
+        String [] lista = ListaSuper.obtenerListado();
         // TODO (5) Cree un loop a través de cada producto y añadalo al TextView (añada \n para darle espacio)
+        for(String producto : lista){
+            listaSuperTextView.append(producto + "\n\n\n");
+
+        }
     }
 }
